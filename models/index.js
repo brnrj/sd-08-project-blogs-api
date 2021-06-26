f3e116754edca44b709b34bc8a38e0b0084ef101
Anderson Santana <anderslash@gmail.com>
2021-06-26 15:35:21 -0300

    refactor: corrige nome da variável environment

--- models/index.js
+++ models/index.js
@@ -2,8 +2,8 @@ const { resolve } = require('path');
 const Sequelize = require('sequelize');
 const config = require(resolve(__dirname, '..', 'config'));
 
-const enviroment = 'development';
+const environment = use_env_variable || 'development';
 
-const connection = new Sequelize(`${config}.${enviroment}`);
+const connection = new Sequelize(`${config}.${environment}`);
 
 module.exports = connection;
