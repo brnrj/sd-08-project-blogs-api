dd6a43b49356f6c8951e9cb378b49ed2114faa2b
Tiago <tiagaoalb@gmail.com>
2021-06-27 18:23:58 -0300

    created src folder

---
+++ src/migrations/20210625211837-create-blog-posts.js
@@ -0,0 +1,24 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('BlogPosts', {
+      id: { allowNull: false, autoIncrement: true, primaryKey: true, type: Sequelize.INTEGER },
+      title: { allowNull: false, type: Sequelize.STRING },
+      content: { allowNull: false, type: Sequelize.STRING },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+      },
+      createdAt: { allowNull: false, type: Sequelize.DATE, field: 'published' },
+      updatedAt: { allowNull: false, type: Sequelize.DATE, field: 'updated' },
+    });
+  },
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('BlogPosts');
+  },
+};
