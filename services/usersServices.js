81b625b4c87683a7854b23d094ea990d9a4f30ab
Fernanda PC <nandateixmarques@gmail.com>
2021-06-27 01:02:35 -0300

    adicionando o requisito 1

---
+++ services/usersServices.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const { User } = require('../models');
+require('dotenv/config');
+const { validateUserEmail } = require('../middlewares/validateForm');
+
+const createUser = async (data) => {
+  const emailExist = await validateUserEmail(data);
+  if (emailExist) throw new Error('User already registered');
+  const newUser = await User.create(data);
+  const { email, password } = newUser;
+  const token = jwt.sign({ email, password }, process.env.JWT_SECRET);
+  return { token };
+};
+
+const getUsersAll = async () => {
+  const usersAll = await User.findAll({ attributes: { exclude: ['password'] } });
+  return usersAll;
+};
+
+module.exports = {
+  createUser,
+  getUsersAll,
+};
