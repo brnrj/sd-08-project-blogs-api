9bcd83461b3611fdc8eafd682fe1df15abfea4c3
Tandy <tandyfw@gmail.com>
2021-06-28 09:57:43 -0300

    Requisito 6 concluido

--- controllers/categoryControllers.js
+++ controllers/categoryControllers.js
@@ -4,13 +4,19 @@ const { tokenValidation } = require('../middlewares');
 
 const router = express.Router();
 
-const BAD_REQUEST = 400;
+const OK = 200;
 const CREATED = 201;
+const BAD_REQUEST = 400;
 router.post('/', tokenValidation, async (req, res) => {
   const { name } = req.body;
   if (!name) return res.status(BAD_REQUEST).json({ message: '"name" is required' });
   const category = await Categories.create(name);
   res.status(CREATED).json({ ...category.dataValues, name });
+});
+
+router.get('/', tokenValidation, async (_req, res) => {
+  const categories = await Categories.findAll();
+  res.status(OK).json(categories);
 });
 
 module.exports = router;
