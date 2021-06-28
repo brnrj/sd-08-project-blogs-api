597fab70ab3d3ce8b14b3d36f9f32d2694c81832
Bruno Wesley <bruno.wesley77@gmail.com>
2021-06-28 18:11:22 -0300

    criar models e migrations

---
+++ models/blogposts.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define(
+    'BlogPost',
+    {
+      id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+      title: DataTypes.STRING,
+      content: DataTypes.STRING,
+    },
+    {
+      timestamps: false,
+      createdAt: 'published',
+      updatedAt: 'updated',
+    },
+  );
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User, { as: 'user', foreignKey: 'userId' });
+  };
+
+  return BlogPost;
+};
