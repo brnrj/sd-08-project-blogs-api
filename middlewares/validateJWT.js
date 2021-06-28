c625ff6a6e333f1ec9a53858e2b7db8f04481dda
Fernanda PC <nandateixmarques@gmail.com>
2021-06-28 10:30:02 -0300

    corrigindo o requisito 3 e 4

--- middlewares/validateJWT.js
+++ middlewares/validateJWT.js
@@ -1,18 +1,15 @@
 const jwt = require('jsonwebtoken');
-const { User } = require('../models');
 const { code } = require('../helpers/messages');
 require('dotenv/config');
 
-const validateJWT = async (req, res, next) => {
+const validateJWT = (req, res, next) => {
   const token = req.headers.authorization;
   try {
     if (!token) {
       res.status(code.UNAUTHORIZED).json({ message: 'Token not found' });
     }
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const user = await User.findOne({ where: { email: decoded.email } });
-    req.user = user;
-    console.log(req.user);
+    const verifyToken = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = verifyToken;
     next();
   } catch (error) {
     return res.status(code.UNAUTHORIZED).json({ message: 'Expired or invalid token' });
