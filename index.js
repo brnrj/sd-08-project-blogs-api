966f32ee2fcd6b0f64c27fac4f94547978f68d49
enicio <jessandro42@gmail.com>
2021-06-28 23:56:10 -0300

    Criada a tabela de Users

--- index.js
+++ index.js
@@ -2,9 +2,20 @@ const express = require('express');
 
 const app = express();
 
+const { User } = require('./models');
+
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
+});
+
+app.get('/users', (req, res) => {
+  User.findAll().then((users) => {
+    res.status(200).json(users);
+  }).catch((err) => {
+    console.log(err);
+    res.status(404).send('ops');
+  });
 });
