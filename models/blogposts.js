1c78cf8163cbbec96fb7b3fdfb780bea5be36ded
JRGCast <joao.ralph1@gmail.com>
2021-06-30 16:17:23 -0300

    Update: adicionado title em models e migrations do blogposts

--- models/blogposts.js
+++ models/blogposts.js
@@ -2,6 +2,7 @@ const BlogPostsModel = (sequelize, DataTypes) => {
   const BlogPosts = sequelize.define('BlogPosts', {
     // id: DataTypes.STRING, // gera automaticamente
     name: DataTypes.STRING,
+    title: DataTypes.STRING,
     content: DataTypes.STRING,
     userId: { type: DataTypes.INTEGER, foreignKey: true }, // A declaração da Foreign Key é opcional no model
     published: DataTypes.DATE,
