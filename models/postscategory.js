a7af66e61ad9248bcd35547fb99cf5430c69e279
JRGCast <joao.ralph1@gmail.com>
2021-06-30 16:19:52 -0300

    Update: adicionado postId no models e migrations de postCategory

--- models/postscategory.js
+++ models/postscategory.js
@@ -1,6 +1,7 @@
 const PostCategoryModel = (sequelize, DataTypes) => {
   const PostCategory = sequelize.define('PostCategory', {
     // id: DataTypes.STRING, // gera automaticamente
+    postId: DataTypes.STRING,
     categoryId: DataTypes.STRING,
   }, {
     timestamps: false,
