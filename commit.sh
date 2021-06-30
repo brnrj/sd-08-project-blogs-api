5a0f055c78f07cb4d3cdf59b9773ca31c734a96a
JRGCast <joao.ralph1@gmail.com>
2021-06-29 22:23:24 -0300

    Update: script pouco mexido, simplificado ainda mais

--- commit.sh
+++ commit.sh
@@ -3,9 +3,9 @@
 # Ask for commit message
 clear
 read -p "enter commit message: " message 
-read -p "commit message will be  >> Update: $message <<. Push to github? " response 
 git add . 
 git commit -m "Update: $message"
+read -p "commited message >> Update: $message <<. Push to github? " response 
 if [[ $response == "y" ]]; then
 git push
 else
