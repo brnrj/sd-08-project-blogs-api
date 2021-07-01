4914b0a7cde60771f2594e6bea135fe700a93547
Rodrigo Leite <rods.leite27@gmail.com>
2021-07-01 20:25:22 -0300

    req 4

--- sequelize/controllers/userControllers.js
+++ sequelize/controllers/userControllers.js
@@ -8,46 +8,34 @@ const router = express.Router();
 router.post('/', validateUserRegister, (req, res) => {
   const { displayName, email, password, image } = req.body;
   Users.create({ displayName, email, password, image })
-    .then((newUser) => {
-      const { password: _, ...UserWithoutPassword } = newUser.dataValues;
-      res.status(201).send(UserWithoutPassword);
+    .then((user) => {
+      const { password: _, ...UserWithoutPassword } = user.dataValues;
+      res.status(201).json(UserWithoutPassword);
     });
-    // .catch((err) => {
-    //   console.log(err.message);
-    //   res.status(500).send({ message: 'Algo deu errado.' });
-    // });
-  // const token = tokenGenerator(UserWithoutPassword);
-  // res.locals.user = token;
 });
 
 router.get('/', validateToken, (req, res) => {
   Users.findAll()
   .then((users) => {
     res.status(200).json(users);
-  })
-  .catch((err) => {
-    console.log(err.message);
-    res.status(500).send({ message: 'Algo deu errado.' });
   });
 });
 
-// router.get('/:id', (req, res) => {
-//   Users.findByPk(req.params.id)
-//     .then((user) => {
-//       if (user === null) {
-//         res.status(404).send({ message: 'Usuário não encontrado.'})
-//       }
-//       if (!req.query.includeProducts) res.status(200).json(user);
-//       return user.getProducts().then((products) => {
-//         // res.status(200).json({ ...user, products });
-//         res.status(200).json({ user, products });
-//       })
-//     })
-//     .catch((err) => {
-//       console.log(err.message);
-//       res.status(500).send({ message: "Algo deu errado." });
-//     })
-// })
+router.get('/:id', validateToken, (req, res) => {
+  Users.findByPk(req.params.id)
+    .then((user) => {
+      if (!user) {
+        res.status(404).send({ message: 'User does not exist' });
+      }
+      const { password: _, ...UserWithoutPassword } = user.dataValues;
+      
+      res.status(200).json(UserWithoutPassword);
+    })
+   .catch((e) => {
+    console.log(e.message);
+    res.status(500).send({ message: 'Algo deu errado' });
+  });
+});
 
 // router.delete('/:id', (req, res) => {
 //   Users.destroy({
