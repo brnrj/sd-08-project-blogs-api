e8601a7582e0cf7ae4e0d1bfe88e259150e14ffe
Seu nome <luiza.dilly@gmail.com>
2021-07-01 20:21:12 -0300

    passou :D

--- services/blogPost.js
+++ services/blogPost.js
@@ -1,4 +1,4 @@
-const { BlogPost } = require('../models/index.js');
+const { BlogPost, Category } = require('../models/index.js');
 
 const isValidBlogPost = (title, content, categoryIds) => {
   if (!title) return '"title" is required';
@@ -7,28 +7,22 @@ const isValidBlogPost = (title, content, categoryIds) => {
   return undefined;
 };
 
-// const findAllCat = async (categoryIds) => {
-//   const findCat = await Category.findAll();
-//   const catIds = findCat.map((cat) => cat.id);
-//   const trueCat = categoryIds.every((validCatId) => catIds.includes(validCatId));
-//   return trueCat;
-// };
+const findAllCat = async (categoryIds) => {
+  const findCat = await Category.findAll();
+  const catIds = findCat.map((cat) => cat.id);
+  const trueCat = categoryIds.every((validCatId) => catIds.includes(validCatId));
+  return trueCat;
+};
 
 const createBlogPost = async (title, content, categoryIds, userId) => {
   const validBlogPost = isValidBlogPost(title, content, categoryIds);
   
   if (validBlogPost) throw new Error(validBlogPost);
 
-  // const findBlogPost = await findAllCat(categoryIds);
+  const findBlogPost = await findAllCat(categoryIds);
   
-  // if (!findBlogPost) throw new Error('"categoryIds" not found');
-
-  const newBlog = (await BlogPost.create({ title, content, userId }));
-  try {
-    newBlog.addCategories(categoryIds);
-  } catch (e) {
-    throw new Error('"categoryIds" not found');
-  }
+  if (!findBlogPost) throw new Error('"categoryIds" not found');
+  const newBlog = await BlogPost.create({ title, content, userId });
 
   return newBlog;
 };
