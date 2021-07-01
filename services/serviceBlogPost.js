8b67c91dd4529a4988b4d7791da3b1de2a3a7f6b
Victor-WLS2 <victorfelipeoliveira1@gmail.com>
2021-07-01 15:35:48 -0300

    fix req 7

--- services/serviceBlogPost.js
+++ services/serviceBlogPost.js
@@ -1,10 +1,6 @@
-const Sequelize = require('sequelize');
 const { BlogPost, Categorie, PostsCategories } = require('../models');
 const schema = require('../schema');
 const CustomError = require('../utils/customError');
-const config = require('../config/config');
-
-const sequelize = new Sequelize(config.development);
 
 const { err, code, msg } = new CustomError();
 
@@ -21,7 +17,7 @@ const checkListForAddingNewPost = async (data) => {
   return true;
 };
 
-const addPostsCategories = async (postId, categoryIds, t) => {
+const addPostsCategories = async (postId, categoryIds) => {
   if (!Array.isArray(categoryIds)) {
     return PostsCategories.create(
       { postId, categoryId: categoryIds },
@@ -31,19 +27,17 @@ const addPostsCategories = async (postId, categoryIds, t) => {
   const value = categoryIds.reduce((cur, prev) => (
     cur.concat({ postId, categoryId: Number(prev) })
   ), []);
-  await PostsCategories.create(value, { transaction: t });
-  await t.commit();
+  await PostsCategories.create(value);
 };
 
 const addPost = async (data) => {
-  const t = await sequelize.transaction();
   const result = checkListForAddingNewPost(data);
   const { userId, title, content, categoryIds } = data;
   if (result.err) return result;
   const blogPost = await BlogPost.create({
     title, content, userId, published: new Date(), updated: new Date(),
-  }, { transaction: t });
-  await addPostsCategories(blogPost.id, categoryIds, t);
+  });
+  await addPostsCategories(blogPost.id, categoryIds);
   return { blogPost };
 };
 
