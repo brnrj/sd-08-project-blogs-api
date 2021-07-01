122762f002377b40b093f62a1f9fa62f73dbc95d
Digo-Castro <digo_castro@outlook.com>
2021-07-01 14:17:12 -0300

    [Digo-Castro][Blog API] - Req 8 - OK

--- controllers/BlogPost.js
+++ controllers/BlogPost.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const { BlogPost, User } = require('../models');
-const { STATUS_201, ERRORS } = require('../utils/dictionary');
+const { BlogPost, User, Category, PostCategory } = require('../models');
+const { STATUS_201, ERRORS, STATUS_200 } = require('../utils/dictionary');
 const { decoded } = require('../utils/token');
 const authentication = require('../validations/Auth/authentication');
 const postValidation = require('../validations/BlogPost/postValidation');
@@ -14,7 +14,7 @@ router.post('/',
   validationCategory,
   async (req, res) => {
     const { e500 } = ERRORS;
-    const { title, content } = req.body;
+    const { title, content, categoryIds } = req.body;
     const { authorization } = req.headers;
     const userData = decoded(authorization);
     const userEmail = userData.user.email;
@@ -22,16 +22,35 @@ router.post('/',
     try {
       const user = await User.findOne({ where: { email: userEmail } });
       const userId = await user.dataValues.id;
-      console.log(userId);
       const newPost = await BlogPost.create({ title, content, userId });
+      const newPostId = await newPost.dataValues.id;
+
+      await (categoryIds.map(async (id) => PostCategory && PostCategory.create({ newPostId, id })));
+
       return res.status(STATUS_201).json(newPost.dataValues);
     } catch (err) {
       return res.status(e500.status).json({ message: e500.message });
     }
   });
 
-  router.get('/', authentication, async (_req, _res) => {
-
+  router.get('/', authentication, async (_req, res) => {
+    const { e500 } = ERRORS;
+    try {
+      const allPosts = await BlogPost.findAll({
+        include: [{
+            model: User,
+            as: 'user',
+            attributes: ['id', 'displayName', 'email', 'image'],
+          },
+          {
+            model: Category,
+            as: 'categories',
+            attributes: ['id', 'name'],
+          }] });
+      return res.status(STATUS_200).json(allPosts);
+    } catch (err) {
+      return res.status(e500.status).json({ message: e500.message });
+    }
   });
 
 module.exports = router;
---
+++ models/PostCategory.js
@@ -0,0 +1,22 @@
+const definePostCategoryModel = (sequelize, _DataTypes) => {
+  const PostCategory = sequelize.define('PostsCategories', {}, { timestamps: false });
+
+  PostCategory.associate = (models) => {
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'posts',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'categories',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+  };
+
+  return PostCategory;
+};
+
+module.exports = definePostCategoryModel;
