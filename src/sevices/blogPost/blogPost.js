2583fe045ae2d6d0f562cfa1a2691cfaeb080f5a
jnpefo <jnpefo@gmail.com>
2021-07-02 18:19:46 -0300

    fiz 8

--- src/sevices/blogPost/blogPost.js
+++ src/sevices/blogPost/blogPost.js
@@ -18,42 +18,13 @@ const createServices = async (data) => {
 };
 
 const findServices = async () => {
-  try {
-    const result1 = await BlogPost.findAll({
-      include: [
-        { model: User, as: 'user', attributes: { exclude: ['password'] } },
-        { model: Categories, as: 'categories', through: { attributes: [] } },
-      ],
-    });
-    console.log(result1);
-    return result1;
-    
-  } catch (error) {
-    console.log(error);
-  }
-    // return result;
-  // [
-  //   {
-  //     "id": 1,
-  //     "title": "Post do Ano",
-  //     "content": "Melhor post do ano",
-  //     "userId": 1,
-  //     "published": "2011-08-01T19:58:00.000Z",
-  //     "updated": "2011-08-01T19:58:51.000Z",
-  //     "user": {
-  //       "id": 1,
-  //       "displayName": "Lewis Hamilton",
-  //       "email": "lewishamilton@gmail.com",
-  //       "image": "https://upload.wikimedia.org/wikipedia/commons/1/18/Lewis_Hamilton_2017_Malaysia.jpg"
-  //     },
-  //     "categories": [
-  //       {
-  //         "id": 1,
-  //         "name": "Inovação"
-  //       }
-  //     ]
-  //   }
-  // ]
+  const result1 = await BlogPost.findAll({
+    include: [
+      { model: User, as: 'user', attributes: { exclude: ['password'] } },
+      { model: Categories, as: 'categories', through: { attributes: [] } },
+    ],
+  });
+  return result1;
 };
 
 module.exports = {
