d005d3561d917357f6a5ca3adc97602dd64961a8
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-07-03 16:14:46 -0300

    cria funçao checkPost no BlogPostsController

---
+++ controllers/BlogPostController.js
@@ -0,0 +1,20 @@
+const BAD_REQUEST = 400;
+
+const checkPost = (req, res, next) => {
+  const { title, content, categoryIds } = req.body;
+  if (!title) {
+    return res.status(BAD_REQUEST).json({ message: '"title" is required' });
+  }
+  if (!content) {
+    return res.status(BAD_REQUEST).json({ message: '"content" is required' });
+  }
+  if (!categoryIds) {
+    return res.status(BAD_REQUEST).json({ message: '"categoryIds" is required' });
+  }
+  next();
+};
+
+const checkCategoryIds = (req, res, next) => {
+  const { categoryIds } = req.body;
+  
+}
