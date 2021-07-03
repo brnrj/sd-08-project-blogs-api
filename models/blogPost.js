f0f6ec3cd8461c20f463d15e0d2b9b02ef7fb964
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-07-03 10:24:24 -0300

    cria model BlogPost

---
+++ models/blogPost.js
@@ -0,0 +1,10 @@
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    userId: DataTypes.INTEGER,
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+
+  }, { timestamps: false });
+
+  return BlogPost;
+};
