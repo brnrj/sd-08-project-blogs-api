368d70944df56c19d2ec7fc651b4f24989e7115a
Silvio Dayube <scarige@gmail.com>
2021-07-04 18:24:55 -0300

    config: Generate migrations

---
+++ migrations/20210704211722-create-post-categories.js
@@ -0,0 +1,28 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('PostCategories', {
+      postId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'BlogPosts',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      categoryId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Categories',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('PostCategories');
+  },
+};
---
+++ models/postcategories.js
@@ -0,0 +1,25 @@
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class PostCategories extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(_models) {
+      // define association here
+    }
+  }
+  PostCategories.init(
+    {
+      postId: DataTypes.INTEGER,
+      categoryId: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: 'PostCategories',
+    },
+  );
+  return PostCategories;
+};
