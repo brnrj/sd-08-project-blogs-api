9c4a4c0fe425d2e7a2ed48fdde8ab46426251c43
Silvio Dayube <scarige@gmail.com>
2021-07-04 19:40:07 -0300

    config: Change models to use 'define' method

--- models/blogposts.js
+++ models/blogposts.js
@@ -1,28 +1,8 @@
-const { Model } = require('sequelize');
-
-module.exports = (sequelize, DataTypes) => {
-  class BlogPosts extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(_models) {
-      // define association here
-    }
-  }
-  BlogPosts.init(
-    {
-      title: DataTypes.STRING,
-      content: DataTypes.STRING,
-      userId: DataTypes.STRING,
-      published: DataTypes.DATE,
-      updated: DataTypes.DATE,
-    },
-    {
-      sequelize,
-      modelName: 'BlogPosts',
-    },
-  );
-  return BlogPosts;
-};
+module.exports = (sequelize, DataTypes) =>
+  sequelize.define('BlogPosts', {
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    userId: DataTypes.STRING,
+    published: DataTypes.DATE,
+    updated: DataTypes.DATE,
+  });
--- models/postcategories.js
+++ models/postcategories.js
@@ -1,25 +1,5 @@
-const { Model } = require('sequelize');
-
-module.exports = (sequelize, DataTypes) => {
-  class PostCategories extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(_models) {
-      // define association here
-    }
-  }
-  PostCategories.init(
-    {
-      postId: DataTypes.INTEGER,
-      categoryId: DataTypes.INTEGER,
-    },
-    {
-      sequelize,
-      modelName: 'PostCategories',
-    },
-  );
-  return PostCategories;
-};
+module.exports = (sequelize, DataTypes) =>
+  sequelize.define('PostCategories', {
+    postId: DataTypes.INTEGER,
+    categoryId: DataTypes.INTEGER,
+  });
