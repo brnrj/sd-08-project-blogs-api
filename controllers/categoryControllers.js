f14e6eb7a4bee701ad4229fab1fdb32ddcf57402
Wesley Paulo Ribeiro <wesley_paulo00@hotmail.com>
2021-07-04 17:24:27 -0300

    R06 - Concluido

--- controllers/categoryControllers.js
+++ controllers/categoryControllers.js
@@ -6,12 +6,18 @@ const router = express.Router();
 
 const BAD_REQUEST = 400;
 const CREATED = 201;
+const OK = 200;
 
 router.post('/', getToken, async (req, res) => {
     const { name } = req.body;
     if (!name) return res.status(BAD_REQUEST).json({ message: '"name" is required' });
     const category = await Category.create(name);
     res.status(CREATED).json({ ...category.dataValues, name });
+});
+
+router.get('/', getToken, async (req, res) => {
+    const categories = await Category.findAll();
+    res.status(OK).json(categories);
 });
 
 module.exports = router;
