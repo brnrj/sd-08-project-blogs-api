19e8e710d1af6ba80e49ffeb7ed245ed73f4c2cb
MEianelli <filipelouzano@gmail.com>
2021-07-04 12:37:44 -0300

    filipe-louzano-blogs-api zerado

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 require('dotenv/config');
 const express = require('express');
-const users = require('./routes/user');
-const categories = require('./routes/category');
-const blogPost = require('./routes/blogPost');
+
 
 const app = express();
 app.use(express.json());
-app.use('/', users);
-app.use('/', categories);
-app.use('/', blogPost);
+
+
 
 const PORT = process.env.PORT || 3000;
 
