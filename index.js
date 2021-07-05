d8217aa00e21331f3e22ca0f43852b777014b83a
MEianelli <filipelouzano@gmail.com>
2021-07-05 09:46:18 -0300

    Todos requisitos

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 require('dotenv/config');
 const express = require('express');
-
+const users = require('./routes/user');
+const categories = require('./routes/category');
+const blogPost = require('./routes/blogPost');
 
 const app = express();
 app.use(express.json());
-
-
+app.use('/', users);
+app.use('/', categories);
+app.use('/', blogPost);
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`ouvindo porta ${PORT}!`));
 
-
-//asdadasd
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
