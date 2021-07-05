fdf697c7786cb63ce996ad3f950a877e9bac943a
devFelipeFranca <felipefrancafj@gmail.com>
2021-07-05 19:14:26 -0300

    Update

---
+++ migrations/20210629193852-create-user.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const User = queryInterface.createTable('Users', {
+        "id": {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER
+        },
+        "displayName": {
+          allowNull: false,
+          type: Sequelize.STRING
+        },
+        "email": {
+          allowNull: false,
+          type: Sequelize.STRING
+        },
+        "password": {
+          allowNull: false,
+          type: Sequelize.STRING
+        },
+        "image": {
+          allowNull: false,
+          type: Sequelize.STRING
+        }
+    },
+    {timestamps: false},
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+   queryInterface.dropTable('Users');
+  }
+};
