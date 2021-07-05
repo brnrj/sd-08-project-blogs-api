1de3059716654c27c906ddd63a56a594404e685f
Rita Jeveaux <ritajeveaux@gmail.com>
2021-07-05 14:45:53 -0300

    Requisito2

--- services/auth.js
+++ services/auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const STATUS_401 = 401;
 
 const auth = async (req, res, next) => {
-  const token = req.headers; 
+  const token = req.headers.authorization;
   if (!token) return res.status(STATUS_401).json({ message: 'Token not found' });
   try {
     const decode = jwt.verify(token, process.env.JWT_SECRET);
