4d5573752746a25e3f690ecc948dc96c3f08ae4a
lucasbgama <lucasbg@protonmail.com>
2021-07-05 22:50:37 -0300

    requisito 6

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -6,12 +6,18 @@ const router = express.Router();
 
 const BAD_REQUEST = 400;
 const CREATED = 201;
+const OK = 200;
 
 router.post('/', authenticator, async (req, res) => {
   const { name } = req.body;
   if (!name) return res.status(BAD_REQUEST).send({ message: '"name" is required' });
   const category = await Categories.create({ name });
   res.status(CREATED).json(category);
+});
+
+router.get('/', authenticator, async (_req, res) => {
+    const categories = await Categories.findAll();
+    return res.status(OK).json(categories);
 });
 
 module.exports = router;
