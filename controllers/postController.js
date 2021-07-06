6bfbffb09e70d9f6c3980ec2e8b8d65f94a90d84
Ana Higo <anahigo.arq@gmail.com>
2021-07-06 13:57:53 -0300

    Testes POST(8, 9, 10, 11 e 13 nao estao passando, Lint- Ok!

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,27 +1,27 @@
-// const { Router } = require('express');
+const { Router } = require('express');
 
-// const BlogPosts = require('../models');
-// const Users = require('../models');
-// const { validateToken } = require('../auth/token');
-// const { 
-//   postValidation,
-//   categoryValidation,
-// } = require('../middlewares/postValidation');
+const { BlogPosts } = require('../models');
+const { Users } = require('../models');
+const { validateToken } = require('../auth/token');
+const { 
+  postValidation,
+  categoryValidation,
+} = require('../middlewares/postValidation');
 
-// const router = new Router();
+const router = new Router();
 
-// // Criar post
-// router.post('/', validateToken, postValidation, categoryValidation, async (req, res) => {
-//   const { title, content, categoryIds } = req.body;
-//   const { email } = req.user;
+// Criar post
+router.post('/', validateToken, postValidation, categoryValidation, async (req, res) => {
+  const { title, content, categoryIds } = req.body;
+  const { email } = req.user;
 
-//   const user = await Users.findOne({ where: { email } });
+  const user = await Users.findOne({ where: { email } });
 
-//   const userId = user.dataValues.id;
+  const userId = user.dataValues.id;
 
-//   const post = await BlogPosts.create({ userId, title, content, categoryIds });
+  const post = await BlogPosts.create({ userId, title, content, categoryIds });
 
-//   return res.status(201).json(post);
-// });
+  return res.status(201).json(post);
+});
 
-// module.exports = router;
+module.exports = router;
