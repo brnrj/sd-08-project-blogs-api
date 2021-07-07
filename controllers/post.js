4728e427e7dc47a72febffb5e8173254b53a8ffc
Rita Jeveaux <ritajeveaux@gmail.com>
2021-07-07 10:04:15 -0300

    Req07

--- controllers/post.js
+++ controllers/post.js
@@ -10,19 +10,21 @@ const STATUS_201 = 201;
 // const STATUS_200 = 200;
 
 PostController.post('/', auth, validBlogPost, validCategoryIds, async (req, res) => {
-  const { title, content, categoryIds } = req.body;
+  const { title, content } = req.body;
   const { email } = req.user;
   // console.log('>>', email);
   const user = await User.findOne({ where: { email } });
-  console.log('>>>', user);
+  // console.log('>>>', user);
    const blogPost = await BlogPost
     .create({ title, content, userId: user.id, published: new Date(), updated: new Date() });
   // console.log('>>>>>', blogPost);
   const allCategories = await Category.findAll();
   const allIds = allCategories.map(({ id }) => id);
-  for (let i = 0; i < categoryIds.length; i += 1) {
-    Promise.all(PostsCategories.create({ categoryIds: allIds[i], postId: blogPost.id }));
-  }
+  await allIds.forEach((catId) => PostsCategories
+    .create({ categoryId: catId, postId: blogPost.id }));
+  // for (let i = 0; i < categoryIds.length; i += 1) {
+  //   Promise.all(PostsCategories.create({ categoryIds: allIds[i], postId: blogPost.id }));
+  // }
   return res.status(STATUS_201).json(blogPost);
 });
 
