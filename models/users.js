727577008f23dc672c978ccba8658e9f011e6929
Blucilha <blucilha.nascimento@gmail.com>
2021-07-08 18:44:13 -0300

    configurando o sequelize

---
+++ models/users.js
@@ -0,0 +1,23 @@
+const {
+  Model,
+} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Users extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(_models) {
+      // define association here
+    }
+  }
+  Users.init({
+    id: DataTypes.NUMBER,
+  }, {
+    sequelize,
+    modelName: 'Users',
+  });
+  return Users;
+};
