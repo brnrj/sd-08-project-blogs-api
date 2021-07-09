f479d41fa893bf147583decf56c2d840247b184a
José Bernardo Johann <josebernardojohann@gmail.com>
2021-07-09 15:40:10 -0300

    Adds list all users route

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ const routes = require('express').Router();
 const users = require('./users');
 const login = require('./login');
 
+routes.get('/user', users);
 routes.post('/user', users);
 routes.post('/login', login);
 
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,7 @@
 const routes = require('express').Router();
 const UsersController = require('../controllers/Users');
 
+routes.get('/user', UsersController.index);
 routes.post('/user', UsersController.create);
 
 module.exports = routes;
