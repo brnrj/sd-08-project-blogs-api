aa1a953a5c03a95c52a7d7a02acf216383b70b07
José Bernardo Johann <josebernardojohann@gmail.com>
2021-07-09 20:34:54 -0300

    Changes sessions JWT payload to users IDs

--- src/controllers/Sessions.js
+++ src/controllers/Sessions.js
@@ -14,7 +14,7 @@ module.exports = {
       const user = await User.findOne({ where: { email } });
       if (!user) throw new InvalidFieldsError();
       passwordValidator(password);
-      return response.status(200).send({ token: tokenGenerator(email) });
+      return response.status(200).send({ token: tokenGenerator(user.id) });
     } catch (err) {
       console.error(`${err.name}`, `${err.message}`);
       return response.status(err.statusCode).send({ message: err.message });
