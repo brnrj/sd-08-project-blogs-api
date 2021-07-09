e1d3ece4312f940582fcaaa07806d5e40db79324
José Bernardo Johann <josebernardojohann@gmail.com>
2021-07-09 20:31:30 -0300

    Connects Posts model to Sequelize

--- src/database/sequelize.js
+++ src/database/sequelize.js
@@ -2,9 +2,13 @@ const Sequelize = require('sequelize');
 const environment = require('./config/environment.js');
 const Users = require('./models/Users');
 const Categories = require('./models/Categories');
+const BlogPosts = require('./models/BlogPosts');
 
 const sequelize = new Sequelize(environment.development);
 Users.init(sequelize);
 Categories.init(sequelize);
+BlogPosts.init(sequelize);
+Users.associate(sequelize.models);
+BlogPosts.associate(sequelize.models);
 
 module.exports = sequelize;
