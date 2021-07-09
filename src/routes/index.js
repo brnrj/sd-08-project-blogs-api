e1ad42ee121f3fffded7cbeaba4342c1485a120e
José Bernardo Johann <josebernardojohann@gmail.com>
2021-07-09 15:50:05 -0300

    Adds a route to list a user by ID

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const users = require('./users');
 const login = require('./login');
 
 routes.get('/user', users);
+routes.get('/user/:id', users);
 routes.post('/user', users);
 routes.post('/login', login);
 
--- src/routes/users.js
+++ src/routes/users.js
@@ -2,6 +2,7 @@ const routes = require('express').Router();
 const UsersController = require('../controllers/Users');
 
 routes.get('/user', UsersController.index);
+routes.get('/user/:id', UsersController.show);
 routes.post('/user', UsersController.create);
 
 module.exports = routes;
