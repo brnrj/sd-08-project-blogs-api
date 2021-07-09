f01f727195b9d872ae5bcd4e656247eb3554c193
José Bernardo Johann <josebernardojohann@gmail.com>
2021-07-09 16:06:48 -0300

    Connects Categories model to Sequelize

--- src/database/sequelize.js
+++ src/database/sequelize.js
@@ -1,8 +1,10 @@
 const Sequelize = require('sequelize');
 const environment = require('./config/environment.js');
 const Users = require('./models/Users');
+const Categories = require('./models/Categories');
 
 const sequelize = new Sequelize(environment.development);
 Users.init(sequelize);
+Categories.init(sequelize);
 
 module.exports = sequelize;
