2c2d74a3d56d0661dea5fb494ad6dd2c92df8811
José Bernardo Johann <josebernardojohann@gmail.com>
2021-07-09 15:39:51 -0300

    Adds index method to list all users

--- src/controllers/Users.js
+++ src/controllers/Users.js
@@ -4,6 +4,7 @@ const {
   emailValidator,
   passwordValidator,
   tokenGenerator,
+  tokenValidator,
 } = require('../utils/helpers');
 const { UserAlreadyExistsError } = require('../utils/errors');
 
@@ -19,6 +20,18 @@ module.exports = {
       await User.create({ displayName, email, password, image });
       const accessToken = tokenGenerator(email);
       return response.status(201).send({ token: accessToken });
+    } catch (err) {
+      console.error(`${err.name}`, `${err.message}`);
+      return response.status(err.statusCode).send({ message: err.message });
+    }
+  },
+  
+  async index(request, response) {
+    try {
+      const { authorization } = request.headers;
+      tokenValidator(authorization);
+      const users = await User.findAll();
+      return response.status(200).send(users);
     } catch (err) {
       console.error(`${err.name}`, `${err.message}`);
       return response.status(err.statusCode).send({ message: err.message });
