eaffebb3dd3646b517705ae6e7facdb8916d4850
José Bernardo Johann <josebernardojohann@gmail.com>
2021-07-10 11:02:04 -0300

    Fixes Users table name in reference

--- src/database/migrations/20210709191312-BlogPosts.js
+++ src/database/migrations/20210709191312-BlogPosts.js
@@ -9,7 +9,7 @@ module.exports = {
     userId: {
       type: Sequelize.INTEGER,
       allowNull: false,
-      references: { model: 'users', key: 'id' },
+      references: { model: 'Users', key: 'id' },
       onUpdate: 'CASCADE',
       onDelete: 'CASCADE',
     },
