d09df0efbc74a928bc44aceb68ace0c466549073
José Bernardo Johann <josebernardojohann@gmail.com>
2021-07-09 21:05:22 -0300

    Adds delete method to delete a post

--- src/controllers/Posts.js
+++ src/controllers/Posts.js
@@ -6,6 +6,7 @@ const {
   categoryIdsValidator,
   decodeToken,
 } = require('../utils/helpers');
+const { UnauthorizedUserError, PostDoesNotExistsError } = require('../utils/errors');
 
 module.exports = {
   async create(request, response) {
@@ -19,6 +20,23 @@ module.exports = {
       const id = decodeToken(authorization);
       const post = await Post.create({ title, content, userId: Number(id) });
       return response.status(201).send(post);
+    } catch (err) {
+      console.error(`${err.name}`, `${err.message}`);
+      return response.status(err.statusCode).send({ message: err.message });
+    }
+  },
+
+  async delete(request, response) {
+    try {
+      const { authorization } = request.headers;
+      const { id } = request.params;
+      tokenValidator(authorization);
+      const decodedUserId = decodeToken(authorization);
+      const post = await Post.findByPk(id);
+      if (!post) throw new PostDoesNotExistsError();
+      if (post.userId !== Number(decodedUserId)) throw new UnauthorizedUserError();
+      await Post.destroy({ where: { id } });
+      return response.status(204).send();
     } catch (err) {
       console.error(`${err.name}`, `${err.message}`);
       return response.status(err.statusCode).send({ message: err.message });
