18cee8c921c8667c13effebb76bd2c23ccbde461
José Bernardo Johann <josebernardojohann@gmail.com>
2021-07-10 11:09:23 -0300

    Changes models exportation

---
+++ src/database/models/index.js
@@ -0,0 +1,9 @@
+const Users = require('./Users');
+const Categories = require('./Categories');
+const BlogPosts = require('./BlogPosts');
+
+module.exports = {
+  Users,
+  Categories,
+  BlogPosts,
+};
