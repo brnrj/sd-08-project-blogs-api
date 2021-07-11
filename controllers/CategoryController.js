de1f5de89df208fba5da5a9ee5ab2dce92b3ecd9
Luis Felipe Avila <lfelipeavila@outlook.com>
2021-07-11 02:08:17 -0300

    req6

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -4,9 +4,23 @@ const { Category } = require('../models');
 
 const router = express.Router();
 
+const httpRequestOk = 200;
 const httpRequestSubmit = 201;
 const httpRequestErr = 401;
 const httpRequestErro = 400;
+
+router.get('/', async (req, res) => {
+  const JwtSecret = 'secret';
+  const token = req.headers.authorization;
+
+  if (!token) res.status(httpRequestErr).json({ message: 'Token not found' });
+
+  jwt.verify(token, JwtSecret, async (err) => {
+    if (err) return res.status(httpRequestErr).json({ message: 'Expired or invalid token' });
+    const category = await Category.findAll();
+    res.status(httpRequestOk).json(category);
+  });
+});
 
 router.post('/', async (req, res) => {
   const JwtSecret = 'secret';
