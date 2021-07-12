73879e05b2797632a2ee6bbbe48f38773672ad0b
Daniel <marcio.daniel@msn.com>
2021-07-12 20:33:40 -0300

    req2

--- app/app.js
+++ app/app.js
@@ -3,11 +3,10 @@ const { usersRoute, loginRoute } = require('./routes');
 const errorHandler = require('../middlewares/errorHandler');
 
 const app = express();
+app.get('/', (_req, res) => res.send());
 app.use(express.json());
 app.use('/user', usersRoute);
 app.use('/login', loginRoute);
 app.use(errorHandler);
-
-app.get('/', (_req, res) => res.send());
 
 module.exports = app;
