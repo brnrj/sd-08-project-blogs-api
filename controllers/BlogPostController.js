9462ece339a2dca9ff62fd17b4db6540c464a82a
Luis Felipe Avila <lfelipeavila@outlook.com>
2021-07-12 02:24:52 -0300

    req 8

--- controllers/BlogPostController.js
+++ controllers/BlogPostController.js
@@ -1,11 +1,30 @@
 const express = require('express');
-const { BlogPost, Category } = require('../models');
+const jwt = require('jsonwebtoken');
+const { BlogPost, Category, User } = require('../models');
 const { ErrorsBlogPost } = require('../schemas');
 
 const router = express.Router();
 
+const httpRequestOk = 200;
 const httpRequestSubmit = 201;
 const httpRequestError = 400;
+const httpRequestErr = 401;
+
+router.get('/', async (req, res) => {
+  const JwtSecret = 'secret';
+  const token = req.headers.authorization;
+
+  if (!token) return res.status(httpRequestErr).json({ message: 'Token not found' });
+
+  jwt.verify(token, JwtSecret, async (err) => {
+    if (err) return res.status(httpRequestErr).json({ message: 'Expired or invalid token' });
+    const posts = await BlogPost.findAll({ include: [
+      { model: User, as: 'user', attributes: { exclude: ['password'] } },
+      { model: Category, as: 'categories', through: { attributes: [] } },
+    ] });
+    res.status(httpRequestOk).json(posts);
+  });
+});
 
 router.post('/', ErrorsBlogPost, async (req, res) => {
   const { title, content, categoryIds } = req.body;
