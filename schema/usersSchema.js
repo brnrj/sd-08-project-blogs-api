e841dfd27edccdeeb185d40c387bb9814b26a2c8
Daniel <marcio.daniel@msn.com>
2021-07-12 17:16:38 -0300

    req 1

---
+++ schema/usersSchema.js
@@ -0,0 +1,9 @@
+const Joi = require('joi');
+
+const usersCreate = Joi.object({
+  displayName: Joi.string().min(8).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).message('"password" length must be 6 characters long').required(),
+});
+
+module.exports = { usersCreate };
