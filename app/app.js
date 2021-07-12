85644b531d9798b15e05860fda1f43534bdd0f43
Daniel <marcio.daniel@msn.com>
2021-07-12 20:05:08 -0300

    req2

--- app/app.js
+++ app/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { usersRoute } = require('./routes');
+const { usersRoute, loginRoute } = require('./routes');
 const errorHandler = require('../middlewares/errorHandler');
 
 const app = express();
 app.use(express.json());
 app.use('/user', usersRoute);
+app.use('/login', loginRoute);
 app.use(errorHandler);
 
 app.get('/', (_req, res) => res.send());
--- app/routes.js
+++ app/routes.js
@@ -2,9 +2,12 @@ const express = require('express');
 const usersController = require('../controllers/usersController');
 
 const usersRoute = express.Router();
+const loginRoute = express.Router();
 
 usersRoute.post('/', usersController.createUser);
+loginRoute.post('/', usersController.loginUser);
 
 module.exports = { 
   usersRoute,
+  loginRoute,
 };
