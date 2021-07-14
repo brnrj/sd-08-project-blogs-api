5648da176cf450595bb87a9ad96571f5e94beaab
caiomzrocha <caiomzrocha@gmail.com>
2021-07-14 19:34:43 -0300

    [REFACT] models/user.js

--- models/user.js
+++ models/user.js
@@ -1,13 +1,14 @@
 const User = (sequelize, DataTypes) => {
-  const user = sequelize.define('User', {
-    displayName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    image: DataTypes.STRING,
-  },
-  {
-    timestamps: false,
-  });
+  const user = sequelize.define(
+    'User',
+    {
+      displayName: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      image: DataTypes.STRING,
+    },
+    { timestamps: false },
+  );
 
   return user;
 };
