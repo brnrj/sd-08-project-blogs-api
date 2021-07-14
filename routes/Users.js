21a85c1f9ae334acce9d39fa2e7423850dee5389
Fabio <fabiohigoralm2@gmail.com>
2021-07-14 15:25:59 -0300

    Req 4

--- routes/Users.js
+++ routes/Users.js
@@ -6,5 +6,5 @@ const router = express.Router();
 
 router.post('/', UsersControllers.createUser);
 router.get('/', validate.jwtValidate, UsersControllers.findAll);
-
+router.get('/:id', validate.jwtValidate, UsersControllers.findById);
 module.exports = router;
--- services/Users.js
+++ services/Users.js
@@ -1,11 +1,12 @@
 const boom = require('@hapi/boom');
+const e = require('express');
 const { User } = require('../models');
 
 const createUser = async (user) => {
     try {
     const addUser = await User.create({ ...user });
     return addUser;
-    } catch (e) {
+    } catch (er) {
         throw boom.conflict('User already registered');
     }
 };
@@ -14,4 +15,15 @@ const findAll = async () => {
     const allUsers = await User.findAll();
     return allUsers;
 };
-module.exports = { createUser, findAll };
+
+const findById = async (id) => {
+    try {
+    const idUsers = await User.findOne({ where: { id } });
+    if (!idUsers) throw e;
+    return idUsers;
+    } catch (err) {
+        throw boom.notFound('User does not exist');
+    }
+};
+
+module.exports = { createUser, findAll, findById };
