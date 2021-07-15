21c5bc8221342d09ecc5a06308288044fbbaf648
Fabio Camargos <fabiocmmcm@gmail.com>
2021-07-15 16:10:47 -0300

    Primeiros requisitos finalmente configurados corretamente

---
+++ controllers/userController.js
@@ -0,0 +1,43 @@
+const { Router } = require('express');
+
+const { Users } = require('../models');
+const { userValidation } = require('../middlewares/userValidation.js');
+const { createToken, validateToken } = require('../auth/token');
+
+const router = new Router();
+
+router.post('/', userValidation, async (req, res) => {
+  const { displayName, email, password, image } = req.body;
+
+  const user = await Users.findOne({ where: { email } });
+
+  if (user) {
+    return res.status(409).json({ message: 'User already registered' });
+  }
+
+  await Users.create({ displayName, email, password, image });
+
+  const token = createToken(email);
+
+  return res.status(201).json({ token });
+});
+
+router.get('/', validateToken, async (_req, res) => {
+  const users = await Users.findAll({});
+
+  return res.status(200).json(users);
+});
+
+router.get('/:id', validateToken, async (req, res) => {
+  const { id } = req.params;
+
+  const user = await Users.findOne({ where: { id } });
+
+  if (!user) {
+    return res.status(404).json({ message: 'User does not exist' });
+  }
+
+  return res.status(200).json(user);
+});
+
+module.exports = router;
