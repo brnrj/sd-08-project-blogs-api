dc74121d7f1385814414cdc65f76a367d43ad717
hugoleonardodev <hugoleonardo.dev@gmail.com>
2021-07-17 19:10:05 -0300

    requisito 08, ok

--- services/post.js
+++ services/post.js
@@ -19,10 +19,38 @@ const createOne = async (newPost) => {
   });
 };
 
-// const findAll = async () => PostModel.findAll();
+// const findAll = async () => {
+//   const [results] = await sequelize.query(
+//     `SELECT
+//     Post.id, Post.title, Post.content, Post.published,
+//     Post.updated, Post.userId, user.id AS user.id,
+//     user.displayName AS user.displayName, user.email AS user.email,
+//     user.password AS user.password, user.image AS user.image,
+//     categories.id AS categories.id, categories.name AS categories.name,
+//     categories->PostsCategories.CategoryId AS categories.PostsCategories.CategoryId,
+//     categories->PostsCategories.PostId AS categories.PostsCategories.PostId
+//     FROM blogs_api.BlogPosts AS Post LEFT OUTER JOIN Users AS user
+//     ON Post.userId = user.id
+//     LEFT OUTER JOIN (
+//     PostsCategories AS categories->PostsCategories
+//     INNER JOIN Categories AS categories
+//     ON categories.id = categories->PostsCategories.CategoryId
+//     )
+//     ON Post.id = categories->PostsCategories.PostId`,
+//   );
+
+//   return results;
+// };
+
+// const findAll = async () => PostModel.findAll({
+//   include: ['user', 'categories'],
+// });
 
 const findAll = async () => PostModel.findAll({
-  include: ['user', 'categories'],
+  include: [
+    'user',
+    { association: 'categories', through: { attributes: [] } },
+  ],
 });
 
 module.exports = {
